6cd0f64c881b2de085c274c21e481c0cf5b02901
surabhisuresh <surabhi.suresh93@yahoo.in>
2017-04-13 16:15:33 -0400

    First three pages
    Modified home page - added SignIn and SignUp functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+//var signIn = require('./routes/signIn');
 
 var app = express();
 
@@ -25,24 +26,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+//app.use('/signIn',signIn);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 //Connect to MySQL
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-    host     : 'x',
+    host     : 'dbcluster.cluster-cmscsvr5od93.us-east-1.rds.amazonaws.com',
     user     : 'group11',
     password : 'funkymonkey123*',
     database : 'recipe'
@@ -16,6 +16,42 @@ connection.connect();
 router.get('/', function(req, res, next) {
     //res.render('index', { title: 'Recipe Finder'});
     res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+//SIGN-IN PAGE
+router.get('/signIn', function(req, res, next) {
+    //res.render('index', { title: 'Recipe Finder'});
+    res.sendFile(path.join(__dirname, '../', 'views', 'signIn.html'));
+});
+
+//SIGN-UP Page
+router.get('/signUp', function(req, res, next) {
+    //res.render('index', { title: 'Recipe Finder'});
+    res.sendFile(path.join(__dirname, '../', 'views', 'signUp.html'));
+});
+
+//ADMIN Page
+router.get('/admin', function(req, res, next) {
+    //res.render('index', { title: 'Recipe Finder'});
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin.html'));
+});
+
+//USER Page
+
+router.get('/user', function(req, res, next) {
+    //res.render('index', { title: 'Recipe Finder'});
+    res.sendFile(path.join(__dirname, '../', 'views', 'user.html'));
+});
+
+//Validate User
+router.get('/validate/:creden', function(req, res) {
+    console.log("Inside index");
+    var creds = req.params.creden.split('&');
+    connection.query('SELECT role from User where Login="'+creds[0]+'" AND Password ="'+creds[1]+'" ' ,function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+    });
+
 });
 
 //SEARCH BY KEYWORD
@@ -32,14 +68,42 @@ router.get('/get_cat', function(req, res) {
     connection.query('SELECT distinct(category) from recipes' ,function (err, rows, fields) {
         if (err) throw err;
         res.json(rows);
+
+    });
+
+});
+
+router.get('/searchByCat/:cat', function(req, res) {
+    connection.query('SELECT title from recipes where category = "'+ req.params.cat+'"' ,function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+    });
+
+});
+
+//SEARCH BY INGREDIENT
+router.get('/searchByIngr/:istr', function(req, res) {
+    var ingredients = req.params.istr.split('_');
+    var query = 'SELECT title from recipes where ingredients like "%'+ingredients[0]+'%"';
+    if (ingredients[1]!='')
+    {
+        query += ' AND ingredients like "%'+ingredients[1]+'%"';
+        if (ingredients[2]!='')
+        {
+            query += ' AND ingredients like "%'+ingredients[2]+'%"';
+        }
+    }
+    connection.query( query,function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
     });
 
 });
 
 /*router.post('/test/submit', function(req, res, next) {
-    var id = req.body.id;
-    res.redirect('/test/' + id);
-});*/
+ var id = req.body.id;
+ res.redirect('/test/' + id);
+ });*/
 
 
 module.exports = router;
